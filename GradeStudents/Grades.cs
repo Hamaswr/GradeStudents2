3aefc376dec78b98e22e310587ae8fed8ed13cd6
hamaswr <rko.351@hotmail.com>
2019-01-28 03:25:58 +0300

    Add project files.

---
+++ GradeStudents/Grades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeStudents
+{
+    public class Grades
+    {
+        public List<float> grades2 = new List<float>();
+
+        public void AddGrade(float grade)
+        {
+            grades2.Add(grade); 
+        }
+    }
+}
---
+++ GradeStudents/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grades g = new Grades();
+            g.AddGrade(95);
+            g.AddGrade(85);
+            g.AddGrade(90);
+
+            var grades = g.grades2;
+
+            float sum = 0;
+
+            foreach (var grade in grades)
+            {
+                sum += grade;
+            }
+
+            var avg = sum / grades.Count;
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {avg}");
+        }
+    }
+}
